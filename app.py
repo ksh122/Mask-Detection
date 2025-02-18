4a11794c52af4239e3b579d55ae126596cdcc1fb
Kshitiz Agrahari <kshitizvayu@gmail.com>
2025-02-19 01:11:11 +0530

    Removed real time camera and added custom video

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO("best.pt")
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('video.mp4')
+
+# Set a fixed width and height for resizing (adjust as needed)
+TARGET_WIDTH = 640
+TARGET_HEIGHT = 480
 
 while True:
 
     ret, frame = cap.read()
 
     if not ret:
-        print("Failed to grab frame")
+        print("Frames Ended")
         break
 
+    
+    frame = cv2.resize(frame, (TARGET_WIDTH, TARGET_HEIGHT))
     results = model(frame)
-
     # Render results on the frame (this will include bounding boxes and labels)
     frame_with_results = results[0].plot()  # Get the result from the first frame and plot the bounding boxes
 
